dd276b22746a138c33ac661d4ec5bb32667a59be
Kartheek A N <50650441+Kartheek2662@users.noreply.github.com>
2019-07-10 10:41:02 +0530

    Update app.e2e-spec.ts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,9 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
-
 describe('workspace-project App', () => {
   let page: AppPage;
-
   beforeEach(() => {
     page = new AppPage();
   });
